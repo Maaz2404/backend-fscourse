4b5f093ad2eafca86580eea51d17535dd86ff78c
Maaz Ahmad <maazahamd2404@gmail.com>
2025-03-22 22:28:53 +0500

    added cors

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+const cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -88,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

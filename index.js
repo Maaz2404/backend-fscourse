aee990efdeb74d18b4562c915f2965acb248c349
Maaz Ahmad <maazahamd2404@gmail.com>
2025-03-23 13:47:40 +0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+
 let notes = [
   {
     id: '1',
@@ -30,8 +31,8 @@ const requestLogger = (request, response, next) => {
 app.use(express.json())
 app.use(requestLogger)
 const cors = require('cors')
-
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')

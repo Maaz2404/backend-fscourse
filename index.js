c3538fc053fd1dd4adccf2b3c8c154636df52ea1
Maaz Ahmad <maazahamd2404@gmail.com>
2025-04-02 23:15:07 +0500

    Enhance person model validation and error handling; update HTML script source and modify test request

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
+  next(error);
+};
 
-  next(error)
-}
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
--- models/person.js
+++ models/person.js
@@ -15,18 +15,29 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v) && v.length >= 8;
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
 });
 
-
-
-personSchema.set('toJSON',{
-    transform: (document,returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema);

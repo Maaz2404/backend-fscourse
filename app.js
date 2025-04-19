20bfcc1f5f985ad7e75cdd18da3c4fb54411ec95
Maaz Ahmad <maazahamd2404@gmail.com>
2025-04-19 18:44:31 +0500

    feat: replace notes functionality with blogs management, update MongoDB connection, and adjust configuration

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const notesRouter = require('./controllers/notes')
+const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 
 const app = express()
 
-logger.info('connecting to', config.MONGODB_URI)
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URL)
 
 mongoose
-  .connect(config.MONGODB_URI)
+  .connect(config.MONGODB_URL)
   .then(() => {
     logger.info('connected to MongoDB')
   })
@@ -18,11 +19,12 @@ mongoose
     logger.error('error connection to MongoDB:', error.message)
   })
 
-app.use(express.static('dist'))
+
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
 
-app.use('/api/notes', notesRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)

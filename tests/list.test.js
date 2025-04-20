1d9cf13db4fb8adf409239108e00e7cb3d692e90
Maaz Ahmad <maazahamd2404@gmail.com>
2025-04-20 22:19:01 +0500

    chnaages

--- tests/list.test.js
+++ tests/list.test.js
@@ -1,75 +1,112 @@
-const { test, describe } = require('node:test')
+const { test, after,beforeEach, before } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+const app = require('../app')
+const supertest = require('supertest')
 
+const api = supertest(app)
+const mongoose = require('mongoose')
+const Blog = require('../models/blog')
 
-describe('total likes', () => {
-    const listWithOneBlog = [
-      {
-        id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-        likes: 5,
-        
-      }
-    ]
-    const listWithManyBlogs = [
-        {
-            id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 7,
-            
-          },
-          {
-            id: "5a422aa71b54a676234d17f8",
-            title: "Go To Statement Considered Harmful",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-            likes: 5,
-            
-          },
-    ]
+initialBlogs = [
+  {
+    id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+
+  },
+  {
+    id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+}
+)
+test('returns correct amount of blogs',async()=>{
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+    assert.strictEqual(blogs.length, initialBlogs.length)
     
-    test('of empty list is zero', () => {
-        const result = listHelper.totalLikes([])
-        assert.strictEqual(result, 0)
-    })
+})
+
+
+// describe('total likes', () => {
+//     const listWithOneBlog = [
+//       {
+//         id: '5a422aa71b54a676234d17f8',
+//         title: 'Go To Statement Considered Harmful',
+//         author: 'Edsger W. Dijkstra',
+//         url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+//         likes: 5,
+        
+//       }
+//     ]
+//     const listWithManyBlogs = [
+//         {
+//             id: "5a422a851b54a676234d17f7",
+//             title: "React patterns",
+//             author: "Michael Chan",
+//             url: "https://reactpatterns.com/",
+//             likes: 7,
+            
+//           },
+//           {
+//             id: "5a422aa71b54a676234d17f8",
+//             title: "Go To Statement Considered Harmful",
+//             author: "Edsger W. Dijkstra",
+//             url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+//             likes: 5,
+            
+//           },
+//     ]
+    
+//     test('of empty list is zero', () => {
+//         const result = listHelper.totalLikes([])
+//         assert.strictEqual(result, 0)
+//     })
   
-    test('when list has only one blog, equals the likes of that', () => {
-      const result = listHelper.totalLikes(listWithOneBlog)
-      assert.strictEqual(result, 5)
-    })
+//     test('when list has only one blog, equals the likes of that', () => {
+//       const result = listHelper.totalLikes(listWithOneBlog)
+//       assert.strictEqual(result, 5)
+//     })
 
-    test('of a bigger list is calculated right', () => {
-        const result = listHelper.totalLikes(listWithManyBlogs)
-        assert.strictEqual(result,12)
-    })
-  })
-  describe('favorite blog',() => {
-    const blog = [
-        {
-            id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 7,
+//     test('of a bigger list is calculated right', () => {
+//         const result = listHelper.totalLikes(listWithManyBlogs)
+//         assert.strictEqual(result,12)
+//     })
+//   })
+//   describe('favorite blog',() => {
+//     const blog = [
+//         {
+//             id: "5a422a851b54a676234d17f7",
+//             title: "React patterns",
+//             author: "Michael Chan",
+//             url: "https://reactpatterns.com/",
+//             likes: 7,
             
-          },
-          {
-            id: "5a422aa71b54a676234d17f8",
-            title: "Go To Statement Considered Harmful",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-            likes: 5,
+//           },
+//           {
+//             id: "5a422aa71b54a676234d17f8",
+//             title: "Go To Statement Considered Harmful",
+//             author: "Edsger W. Dijkstra",
+//             url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+//             likes: 5,
             
-          },
-    ]
+//           },
+//     ]
 
-    test('favorite blog is one with most likes',() => {
-        const result = listHelper.favouriteBlog(blog)
-        assert.deepStrictEqual(result,blog[0])    
-    })
-  })
+//     test('favorite blog is one with most likes',() => {
+//         const result = listHelper.favouriteBlog(blog)
+//         assert.deepStrictEqual(result,blog[0])    
+//     })
+//   })
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT
-const MONGODB_URL = process.env.MONGODB_URL
+const PORT = process.env.PORT;
+let MONGODB_URL;
 
-module.exports = { MONGODB_URL, PORT }
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URL = process.env.TEST_MONGODB_URL_BLOGS;
+} else {
+  MONGODB_URL = process.env.MONGODB_URL_BLOGS;
+}
+
+export { MONGODB_URL, PORT };
